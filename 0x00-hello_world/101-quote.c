0d8fbbd70c6d030cfd2a5ea12f7622b7a9d0d1ac
Alhassan <alhassantawfiq988@gmail.com>
2022-09-24 14:05:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,5 @@
+
+
 #include <unistd.h>
 
 /**
@@ -7,6 +9,6 @@
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",15-10-19\n", 59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
