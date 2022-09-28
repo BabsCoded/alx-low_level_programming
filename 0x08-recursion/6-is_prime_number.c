82192d5970beaaa7feec6d1cba6ee987df22f3a6
BabsCoded <alhassantawfiq988@gmail.com>
2022-09-28 15:51:16 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,11 +9,11 @@
 */
 int helper(int i, int n)
 {
-if (n % i == 0 && n != 0)
-return (0);
-if (n % i != 0 && i < n)
-return (helper(i + 1, n));
-return (1);
+	if (n % i == 0 && n != i)
+		return (0);
+	if (n % i != 0 && i < n)
+		return (helper(i + 1, n));
+	return (1);
 }
 
 /**
@@ -24,9 +24,9 @@ return (1);
  */
 int is_prime_number(int n)
 {
-int i = 2;
+	int i = 2;
 
-if (n < 2)
-return (0);
-return (helper(i, n));
+	if (n < 2)
+		return (0);
+	return (helper(i, n));
 }
