b2f682119f4f49d221d3e716be48df6bd2f9c75c
BabsCoded <alhassantawfiq988@gmail.com>
2022-10-21 13:06:57 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
- * list_len - returns then number of elements in a list
+ * list_len - returns the number of elements in a list
  * @h: singly linked list
  * Return: number of elements in the list
  */
 
-size_t list_len(const list_h *h)
+size_t list_len(const list_t *h)
 {
 	size_t number_of_nodes;
 
