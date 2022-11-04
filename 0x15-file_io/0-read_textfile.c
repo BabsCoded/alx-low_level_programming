ba153ec006ab40c45248aab2c58cc7f06805bccb
BabsCoded <alhassantawfiq988@gmail.com>
2022-11-04 00:46:06 -0700

    task o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,22 +6,22 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to POSTIX standard output
- * @filename - name of file
- * @letters: number of letters it should read and print
+ * read_textfile - reads a text file and prints it to the POSIX
+ * @filename: name of file to read
+ * @letters: number of letters to be read and printed
  *
- * Return: actual number of letter it could read and print
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t  lenr, lenw;
+	ssize_t lenr, lenw;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fde == -1)
+	if (fd == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
